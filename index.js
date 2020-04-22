e956a65eca73f6bae0aa768ca91d85517b02f671
KEITH ROGERS <asocialant@gmail.com>
2020-04-22 11:49:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -26,3 +26,10 @@ function sayHiToGrandma(string){
   }
 }
 sayHiToGrandma("I AM UPPERCASE");
+
+function sayHiToGrandma(string){
+  if(string ==="I love you, Grandma."){
+    return ("I love you, too.")
+  }
+}
+sayHiToGrandma ("I love you, Grandma.");

d47adcc967540186a79967de636e056a95172b36
KEITH ROGERS <asocialant@gmail.com>
2020-04-22 11:34:54 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,31 @@
+function shout(string){
+  return string.toUpperCase();
+}
+shout("life is what you make it");
+
+function whisper(string){
+  return string.toLowerCase();
+}
+whisper("I will become a great and skillful programmer")
+
+function logShout (string){
+   console.log(string.toUpperCase());
+}
+logShout("I tested this in the firefox console and it passed so what is the problem here");
+
+function logWhisper(string){
+  console.log(string.toLowerCase());
+}
+logWhisper("I think I just spotted my error I didnt do the camelCase")
+
+function sayHiToGrandma(string){
+  if(string===string.toLowerCase);
+  return("I can't hear you!")
+}
+sayHiToGrandma("i can't hear you");
+
+function sayHiToGrandma(string){
+  if(string===string.toUpperCase);
+  return("YES INDEED!");
+}
+sayHiToGrandma("HI");
